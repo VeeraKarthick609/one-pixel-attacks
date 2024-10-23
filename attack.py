858046b7e077ac5f816cdaa7e3ac6d25c9a64842
Veera <veerakarthick609@gmail.com>
2024-10-23 20:40:31 +0530

    reviewed folder structure

--- attack.py
+++ attack.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import numpy as np
 import pandas as pd
 from keras.datasets import cifar10
@@ -8,161 +7,107 @@ import pickle
 import os
 from PIL import Image
 
-# Custom Networks
-from networks.lenet import LeNet
-from networks.pure_cnn import PureCnn
-from networks.network_in_network import NetworkInNetwork
-from networks.resnet import ResNet
-from networks.densenet import DenseNet
-from networks.wide_resnet import WideResNet
-from networks.capsnet import CapsNet
+from networks.resnet import ResNet  # Only ResNet is needed
 
 # Helper functions
 from differential_evolution import differential_evolution
 import helper
 
 class PixelAttacker:
-    def __init__(self, models, data, class_names, dimensions=(32, 32)):
+    def __init__(self, model, data, class_names, dimensions=(32, 32)):
         # Load data and model
-        self.models = models
+        self.model = model
         self.x_test, self.y_test = data
         self.class_names = class_names
         self.dimensions = dimensions
 
-        network_stats, correct_imgs = helper.evaluate_models(self.models, self.x_test, self.y_test)
+        network_stats, correct_imgs = helper.evaluate_models([self.model], self.x_test, self.y_test)
         self.correct_imgs = pd.DataFrame(correct_imgs, columns=['name', 'img', 'label', 'confidence', 'pred'])
         self.network_stats = pd.DataFrame(network_stats, columns=['name', 'accuracy', 'param_count'])
 
-    def predict_classes(self, xs, img, target_class, model, minimize=True):
-        # Perturb the image with the given pixel(s) x and get the prediction of the model
+    def predict_classes(self, xs, img, target_class, minimize=True):
+        # Perturb the image with the given pixel(s) and get the prediction of the model
         imgs_perturbed = helper.perturb_image(xs, img)
-        predictions = model.predict(imgs_perturbed)[:, target_class]
-        # This function should always be minimized, so return its complement if needed
+        predictions = self.model.predict(imgs_perturbed)[:, target_class]
         return predictions if minimize else 1 - predictions
 
-    def attack_success(self, x, img, target_class, model, targeted_attack=False, verbose=False):
+    def attack_success(self, x, img, target_class, targeted_attack=False, verbose=False):
         # Perturb the image with the given pixel(s) and get the prediction of the model
         attack_image = helper.perturb_image(x, img)
-
-        confidence = model.predict(attack_image)[0]
+        confidence = self.model.predict(attack_image)[0]
         predicted_class = np.argmax(confidence)
 
-        # If the prediction is what we want (misclassification or 
-        # targeted classification), return True
         if verbose:
             print('Confidence:', confidence[target_class])
-        if ((targeted_attack and predicted_class == target_class) or
-                (not targeted_attack and predicted_class != target_class)):
-            return True
+        return ((targeted_attack and predicted_class == target_class) or
+                (not targeted_attack and predicted_class != target_class))
 
-    def attack(self, img_id, model, target=None, pixel_count=1,
-               maxiter=75, popsize=400, verbose=False, plot=False):
-        # Change the target class based on whether this is a targeted attack or not
+    def attack(self, img_id, target=None, pixel_count=1, maxiter=75, popsize=400, verbose=False, plot=False):
         targeted_attack = target is not None
         target_class = target if targeted_attack else self.y_test[img_id, 0]
 
         # Define bounds for a flat vector of x,y,r,g,b values
-        # For more pixels, repeat this layout
         dim_x, dim_y = self.dimensions
         bounds = [(0, dim_x), (0, dim_y), (0, 256), (0, 256), (0, 256)] * pixel_count
 
-        # Population multiplier, in terms of the size of the perturbation vector x
         popmul = max(1, popsize // len(bounds))
 
-        # Format the predict/callback functions for the differential evolution algorithm
         def predict_fn(xs):
-            return self.predict_classes(xs, self.x_test[img_id], target_class, model, target is None)
+            return self.predict_classes(xs, self.x_test[img_id], target_class, target is None)
 
         def callback_fn(x, convergence):
-            return self.attack_success(x, self.x_test[img_id], target_class, model, targeted_attack, verbose)
+            return self.attack_success(x, self.x_test[img_id], target_class, targeted_attack, verbose)
 
-        # Call Scipy's Implementation of Differential Evolution
         attack_result = differential_evolution(
             predict_fn, bounds, maxiter=maxiter, popsize=popmul,
             recombination=1, atol=-1, callback=callback_fn, polish=False)
 
-        # Calculate some useful statistics to return from this function
         attack_image = helper.perturb_image(attack_result.x, self.x_test[img_id])[0]
-        prior_probs = model.predict(np.array([self.x_test[img_id]]))[0]
-        predicted_probs = model.predict(np.array([attack_image]))[0]
+        prior_probs = self.model.predict(np.array([self.x_test[img_id]]))[0]
+        predicted_probs = self.model.predict(np.array([attack_image]))[0]
         predicted_class = np.argmax(predicted_probs)
         actual_class = self.y_test[img_id, 0]
         success = predicted_class != actual_class
         cdiff = prior_probs[actual_class] - predicted_probs[actual_class]
 
-        # Show the best attempt at a solution (successful or not)
         if plot:
             helper.plot_image(attack_image, actual_class, self.class_names, predicted_class)
 
-        return [model.name, pixel_count, img_id, actual_class, predicted_class, success, cdiff, prior_probs,
+        return [self.model.name, pixel_count, img_id, actual_class, predicted_class, success, cdiff, prior_probs,
                 predicted_probs, attack_result.x, attack_image]
 
-    def attack_all(self, models, samples=100, pixels=(1, 3, 5), targeted=False,
-                   maxiter=75, popsize=400, verbose=True):
+    def attack_all(self, samples=100, pixels=(1, 3, 5), targeted=False, maxiter=75, popsize=400, verbose=True):
         results = []
-        for model in models:
-            model_results = []
-            valid_imgs = self.correct_imgs[self.correct_imgs.name == model.name].img
-            img_samples = np.random.choice(valid_imgs, samples)
+        valid_imgs = self.correct_imgs.img
+        img_samples = np.random.choice(valid_imgs, samples)
 
-            for pixel_count in pixels:
-                for i, img in enumerate(img_samples):
-                    print(model.name, '- image', img, '-', i + 1, '/', len(img_samples))
-                    targets = [None] if not targeted else range(10)
+        for pixel_count in pixels:
+            for i, img in enumerate(img_samples):
+                print(self.model.name, '- image', img, '-', i + 1, '/', len(img_samples))
+                targets = [None] if not targeted else range(10)
 
-                    for target in targets:
-                        if targeted:
-                            print('Attacking with target', self.class_names[target])
-                            if target == self.y_test[img, 0]:
-                                continue
-                        result = self.attack(img, model, target, pixel_count,
-                                             maxiter=maxiter, popsize=popsize,
-                                             verbose=verbose)
-                        model_results.append(result)
+                for target in targets:
+                    if targeted:
+                        print('Attacking with target', self.class_names[target])
+                        if target == self.y_test[img, 0]:
+                            continue
+                    result = self.attack(img, target, pixel_count,
+                                         maxiter=maxiter, popsize=popsize, verbose=verbose)
+                    results.append(result)
 
-            results += model_results
-            helper.checkpoint(results, targeted)
         return results
 
 if __name__ == '__main__':
-    model_defs = {
-        'lenet': LeNet,
-        'pure_cnn': PureCnn,
-        'net_in_net': NetworkInNetwork,
-        'resnet': ResNet,
-        'densenet': DenseNet,
-        'wide_resnet': WideResNet,
-        'capsnet': CapsNet
-    }
-
-    parser = argparse.ArgumentParser(description='Attack models on Cifar10')
-    parser.add_argument('--model', nargs='+', choices=model_defs.keys(), default=model_defs.keys(),
-                        help='Specify one or more models by name to evaluate.')
-    parser.add_argument('--pixels', nargs='+', default=(1, 3, 5), type=int,
-                        help='The number of pixels that can be perturbed.')
-    parser.add_argument('--maxiter', default=75, type=int,
-                        help='The maximum number of iterations in the differential evolution algorithm before giving up and failing the attack.')
-    parser.add_argument('--popsize', default=400, type=int,
-                        help='The number of adversarial images generated each iteration in the differential evolution algorithm. Increasing this number requires more computation.')
-    parser.add_argument('--samples', default=500, type=int,
-                        help='The number of image samples to attack. Images are sampled randomly from the dataset.')
-    parser.add_argument('--targeted', action='store_true', help='Set this switch to test for targeted attacks.')
-    parser.add_argument('--save', default='networks/results/results.pkl', help='Save location for the results (pickle)')
-    parser.add_argument('--verbose', action='store_true', help='Print out additional information every iteration.')
-
-    args = parser.parse_args()
-
     # Load data and model
     _, test = cifar10.load_data()
     class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-    models = [model_defs[m](load_weights=True) for m in args.model]
+    model = ResNet(load_weights=True)  # Only using ResNet
 
-    attacker = PixelAttacker(models, test, class_names)
+    attacker = PixelAttacker(model, test, class_names)
 
     print('Starting attack')
 
-    results = attacker.attack_all(models, samples=args.samples, pixels=args.pixels, targeted=args.targeted,
-                                  maxiter=args.maxiter, popsize=args.popsize, verbose=args.verbose)
+    results = attacker.attack_all(samples=100, pixels=(1, 3, 5), targeted=False, maxiter=75, popsize=400, verbose=True)
 
     columns = ['model', 'pixels', 'image', 'true', 'predicted', 'success', 'cdiff', 'prior_probs', 'predicted_probs',
                'perturbation', 'perturbed_image']
@@ -189,6 +134,8 @@ if __name__ == '__main__':
         img.save(filename)
     print(f'Saved {len(successful_attacks)} perturbed images to {output_dir}')
 
-    print('Saving to', args.save)
-    with open(args.save, 'wb') as file:
+    # Save full results to pickle
+    results_file = 'results.pkl'
+    with open(results_file, 'wb') as file:
         pickle.dump(results, file)
+    print(f'Saved results to {results_file}')
